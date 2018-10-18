97d4bbe8d34426e3dc4c4345db3dcc3f9742243b
leaojn <arealeaoneto@gmail.com>
2018-10-17 23:20:35 -0300

    Ädicionada novas classes ao admin para criação de agenda do medico

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Especialidade, Medico, Cliente, Consulta, Convenio, Turno
+from .models import Especialidade, Medico, Cliente, Consulta, Convenio, Turno, EscalaTempo, DiaAgenda
 
 
 # Register your models here.
@@ -8,7 +8,7 @@ class EspecialidadeAdmin(admin.ModelAdmin):
     list_display = ('nome',)
     fieldsets = (
         (None, {
-            'fields': ('nome','descricao',)
+            'fields': ('nome',)
         }),
     )
 
@@ -41,6 +41,29 @@ class ConvenioAdmin(admin.ModelAdmin):
         }),
     )
 
+@admin.register(EscalaTempo)
+class EscalaTempoAdmin(admin.ModelAdmin):
+    list_display = ('inicio','fim','dia')
+    fieldsets = (
+        (None, {
+            'fields': ('inicio','fim','dia')
+        }),
+    )
+
+class TurnosInline(admin.TabularInline):
+    model = DiaAgenda.turnos.through
+    extra = 3
+
+@admin.register(DiaAgenda)
+class DiaAgendaAdmin(admin.ModelAdmin):
+    list_display = ('data','medico')
+    inlines = (TurnosInline,)
+    fieldsets = (
+        (None, {
+            'fields': ('data','medico')
+        }),
+    )
+
 
 @admin.register(Cliente)
 class ClienteAdmin(admin.ModelAdmin):
@@ -54,7 +77,7 @@ class ClienteAdmin(admin.ModelAdmin):
 
 @admin.register(Consulta)
 class ConsultaAdmin(admin.ModelAdmin):
-    list_display = ('data', 'inicio', 'convenio', 'cliente', 'medico')
+    list_display = ('convenio', 'cliente', 'medico')
     fieldsets = (
         (None, {
             'fields': ('convenio', 'cliente', 'medico','data','inicio')
