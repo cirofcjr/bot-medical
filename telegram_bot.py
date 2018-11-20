f1ac80d78a3c57d659981310b1ba8a8a21580999
leaojn <arealeaoneto@gmail.com>
2018-11-20 19:35:05 -0300

    modificacoes e novas funcionalidades

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,4 @@
+
 import telepot
 import requests
 
@@ -21,12 +22,15 @@ class Especialidade:
 class Bot():
 
     # cliente = Cliente()
-
-    mensagens_enviadas = []
+    new_cliente = Cliente()
+    var = None
+    cria_usuario = None
+    mensagens_enviadas = {}
     cliente = ""
     especialidade = ""
     dicionario = {}
     m2 = {}
+    contador = 0
     horarios = {}
 
     def inicio_conversa(msg=None):
@@ -56,7 +60,6 @@ class Bot():
         if len(resultado) == 1:
             resultado = resultado[0]
             cliente = Cliente()
-            # print(resultado)
             cliente.pk = resultado['id']
             cliente.nome = resultado['nome']
             cliente.cpf = resultado['cpf']
@@ -64,11 +67,8 @@ class Bot():
             cliente.data_nascimento = resultado['data_nascimento']
             cliente.telefone = resultado['telefone']
             return cliente
-            # data_nascimento = input(
-            #     "Confirme a sua data de data_nascimento:")
-            # if cliente.data_nascimento == data_nascimento:
-            #     print('ok')
-            # return cliente
+        else:
+            return False
 
     def criar_especialidade(self, id):
         r = requests.get(url + "especialidades/")
@@ -118,10 +118,16 @@ class Bot():
                 datas[var] = item['data']
                 var += 1
         data_escolhida = datas[int(id)]
+        dia = data_escolhida.split("/")[0]
+        mes = data_escolhida.split("/")[1]
+        ano = data_escolhida.split("/")[2]
+        dta = ano + "-" + mes + "-" + dia
+        self.dicionario['data'] = dta
+
         final = data_escolhida.split("/")
         entry = "%2F"
         data = final[0] + entry + final[1] + entry + final[2]
-        filtro = url + "especialidade/7/datas/?data=" + data
+        filtro = url + "especialidade/" + especialidade_id + "/datas/?data=" + data
         filtrada = requests.get(filtro)
         horarios_vagos = {}
         horarios = filtrada.json()
@@ -160,6 +166,7 @@ class Bot():
 
     def seleciona_medico(self, id):
         hour = self.horarios[int(id)]
+        print(hour)
         horario = id
         self.dict('horario', hour)
         m2 = self.m2
@@ -179,26 +186,98 @@ class Bot():
             indice += 1
 
         # print(new_dicionario)
-        mensagem = ""
+        mensagem = "Selecione o medico:\n"
         for key, value in new_dicionario.items():
             mensagem += str(key) + "- " + value['nome'] + '\n'
         return mensagem
 
-    def pensa(self, msg=None):
+    def get_medico_selecionado(self, id):
+        hour = self.horarios[int(id)]
+        horario = id
+        m2 = self.m2
+        for key, value in m2[int(horario)].items():
+            # print(key)
+            horario_selecionado = key
+        medicos_disponiveis = m2[int(horario)]
+        var = ""
+        for m in medicos_disponiveis:
+            var = m
+        medicos_para_o_horario = m2[int(horario)][var]
+        # print(medicos_para_o_horario)
+        indice = 1
+        new_dicionario = {}
+        for key, value in medicos_para_o_horario.items():
+            new_dicionario[indice] = {'pk': key, 'nome': value}
+            indice += 1
+        return new_dicionario
 
-        self.mensagens_enviadas.append(msg)
-        if len(self.mensagens_enviadas) == 1:
+    def post_consulta(self):
+        dicionario = self.dicionario
+        data = {
+            "medico": dicionario['medico'],
+            "cliente": dicionario['cliente'],
+            "data": dicionario['data'],
+            "inicio": dicionario['horario']
+        }
+        criar_consulta = requests.post(url + "consultas/", data=data)
+        if criar_consulta.status_code == 201:
+            return "Agendado com sucesso!!!"
+        else:
+            return "Error!!!"
+
+    def criar_cliente(self, msg):
+        cliente = self.new_cliente
+        if cliente.nome == "":
+            self.new_cliente.nome = msg
+            return "Confirme o CPF do cliente:"
+        if cliente.cpf == "":
+            cliente.cpf = msg
+            return "Informe a sua data de nascimento:"
+        if cliente.data_nascimento == "":
+            dia = msg.split("/")[0]
+            mes = msg.split("/")[1]
+            ano = msg.split("/")[2]
+            dta = ano + "-" + mes + "-" + dia
+            cliente.data_nascimento = dta
+            return "Digite o numero do seu telefone com DDD"
+        if cliente.telefone == "":
+            cliente.telefone = msg
+
+            return cliente.nome + " " + cliente.data_nascimento + " " + cliente.telefone
+        self.var = True
+        return "Confirme a data de nascimento"
+
+    def pensa(self, msg=None, chat_id=None):
+
+        self.contador += 1
+        if chat_id not in self.mensagens_enviadas:
+            self.mensagens_enviadas[chat_id] = {}
+        self.mensagens_enviadas[chat_id][self.contador] = msg
+        print(len(self.mensagens_enviadas))
+        if len(self.mensagens_enviadas[chat_id]) == 1:
             return self.inicio_conversa()
-        if len(self.mensagens_enviadas) == 2:
+        if len(self.mensagens_enviadas[chat_id]) == 2:
             # print(msg)
             return self.pedir_cpf(msg)
-        if len(self.mensagens_enviadas) == 3:
-            self.cliente = self.valida_cpf(msg)
-            self.dict('cliente', self.cliente.pk)
-            return "Confirme sua data de nascimento:"
+
+        if len(self.mensagens_enviadas[chat_id]) == 3:
+            if self.var == None:
+                if self.valida_cpf(msg) != False:
+                    self.cliente = self.valida_cpf(msg)
+                    self.dict('cliente', self.cliente.pk)
+                    return "Oi, " + self.cliente.nome + "\nConfirme sua data de nascimento:"
+                else:
+                    self.var = False
+            if self.var == False:
+                self.mensagens_enviadas[chat_id].pop(self.contador)
+                if self.cria_usuario == None:
+                    self.cria_usuario = True
+                    return "Informe o nome do cliente:"
+
+                return self.criar_cliente(msg)
+
         if len(self.mensagens_enviadas) == 4:
-            return self.validar_nascimento(msg, self.cliente.data_nascimento)
-            # True:
+            return self.validar_nascimento(msg, self.new_cliente.data_nascimento)
 
         if len(self.mensagens_enviadas) == 5:
             self.especialidade = self.criar_especialidade(msg)
@@ -209,6 +288,11 @@ class Bot():
             return self.data_escolhida(msg)
         if len(self.mensagens_enviadas) == 8:
             return self.seleciona_medico(msg)
+        if len(self.mensagens_enviadas) == 9:
+            pk_medico = self.get_medico_selecionado(msg)[int(msg)]['pk']
+            self.dicionario['medico'] = pk_medico
+            print(self.dicionario)
+            return self.post_consulta()
 
             # return self.datas_disponiveis_para_especialidade(msg,
             # self.especialidade.pk)
@@ -258,10 +342,6 @@ class Bot():
         #         print('ok')
         #     return cliente
 
-    def teste(self, number):
-        if number == 1:
-            return "Teste"
-
 
 telegram = telepot.Bot("771366635:AAGWjSGgYTyICMq9vC1lMa5yTQt_1YzE2XY")
 
@@ -269,8 +349,9 @@ bot = Bot()
 
 
 def receber_msg(msg):
-    mensagem = bot.pensa(msg['text'])
     id = msg['chat']['id']
+
+    mensagem = bot.pensa(msg['text'], chat_id=id)
     telegram.sendMessage(id, mensagem)
 
     # print(mensagem)
